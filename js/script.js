9731b6b9daff06f8bd87a891391bbe94c4dd055d
guigawiller <guilhermewiller@hotmail.com>
2018-11-25 20:23:53 -0200

    cria função que passa id para alteração

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-var overwatch = {
+var db = {
   "herois": [{
       "nome": "Bastion",
       "funcao": "Dano",
       "dificuldade": 1,
       "idade": 30,
-      "base": "nenhuma",
+      "afiliacao": "nenhuma",
       "nomeVerdadeiro": "Autômato de Cerco E54 dos Laboratórios SST"
     },
     {
@@ -41,31 +41,61 @@ var overwatch = {
     }
   ]
 };
-// converte JSON em Objeto Javascript
-// var overwatch = JSON.parse(textoJSON);
+
+// Caso exista no Local Storage, recupera os dados salvos
+var overwatch = JSON.parse(localStorage.getItem('overwatch'));
+if (!overwatch) {
+    overwatch = db
+};
+
+
 
 function listar() {
   for (var i = 0; i < overwatch.herois.length; i++) {
+    // cria linha
     var tr = document.createElement('tr');
     tr.id = "num" + i;
-
+    // cria campo de número da linha
     var th = document.createElement('th');
-    th.innerHTML = i+1;
-
+    th.innerHTML = i + 1;
+    // cria campo de nome do heroi
     var nome = document.createElement('td');
     nome.innerHTML = overwatch.herois[i].nome;
-
+    // cria campo de função do heroi
     var funcao = document.createElement('td');
     funcao.innerHTML = overwatch.herois[i].funcao;
-
+    // cria campo de dificuldade do heroi
     var dificuldade = document.createElement('td');
     dificuldade.innerHTML = overwatch.herois[i].dificuldade;
+    // cria campo para inserir links
+    var operacoes = document.createElement('td');
+    operacoes.id = "op" + i;
+    // cria link de alterar
+    var a = document.createElement('a');
+    a.id = i;
+    a.href = 'alterar.html';
+    a.innerHTML = "alterar";
+    a.onclick = function passarHeroi(i){
+      sessionStorage.setItem('hero', JSON.stringify(overwatch.herois[this.id]) );
+    };
+    // cria link de excluir
+    var b = document.createElement('a');
+    b.href = 'alterar.html';
+    b.innerHTML = "alterar";
 
     document.getElementById('resultados').appendChild(tr);
     document.getElementById('num' + i).appendChild(th);
     document.getElementById('num' + i).appendChild(nome);
     document.getElementById('num' + i).appendChild(funcao);
     document.getElementById('num' + i).appendChild(dificuldade);
+    document.getElementById('num' + i).appendChild(operacoes);
+    document.getElementById('op' + i).appendChild(a);
   }
+
+}
+
+function alterar(){
+  var hero = JSON.parse(sessionStorage.getItem('hero'));
+  
 
 }
